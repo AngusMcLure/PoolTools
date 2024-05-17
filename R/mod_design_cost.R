0003f05200488663a1433d4a55be00e6639f1dee
Fred Jaya <36436914+fredjaya@users.noreply.github.com>
2024-05-17 14:09:21 +1000

    DOC: document `mod_design_cost.R`

--- R/mod_design_cost.R
+++ R/mod_design_cost.R
@@ -1,3 +1,12 @@
+#' Input and storage of "Design" Costs.
+#'
+#' Most importantly, `value = isolate(r_store[[id]])` ensures that previously
+#' stored values are displayed when the UI changes. `isolate()` breaks the
+#' circular reference to the `r_store`. `cost_server()` updates the relevant
+#' `r_store` key whenever the `numericInput()` changes.
+#'
+#' @param id
+#' @param r_store `reactiveValues()` object that stores costs.
 cost_ui <- function(id, r_store) {
   ns <- NS(id)
   numericInput(ns("cost"), paste0("Cost per ", id), value = isolate(r_store[[id]]), min = 1e-6, step = 0.5)
@@ -8,6 +17,5 @@ cost_server <- function(id, r_store) {
     observeEvent(input$cost, {
       r_store[[id]] <- as.numeric(input$cost)
     }, ignoreNULL = TRUE)
-
   })
 }
