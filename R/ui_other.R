e2405e1b462fa82195ef133b073b9272e41fd90c
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-12-05 12:04:26 +1100

    Deleted all files not needed for static redirect

--- R/ui_other.R
+++
@@ -1,8 +0,0 @@
-processOther <- function(input, inputId) {
-  otherId <- paste0(inputId, "Other")
-  if (input[[inputId]] == "other") {
-    as.numeric(input[[otherId]]) / 100
-  } else {
-    as.numeric(input[[inputId]])
-  }
-}
